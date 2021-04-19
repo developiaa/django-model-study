ca6ff5bde832f8f3d70fb5680f12d29e2d5192be
developiaa <developiaa@gmail.com>
2021-04-19 16:17:54 +0900

    Refactor comment query, exception

--- boards/views/comment.py
+++ boards/views/comment.py
@@ -1,6 +1,7 @@
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from django.db.models import Max
+import json
 
 from model.comment import Comment
 from model.post import Post
@@ -13,23 +14,27 @@ class CommentView(ModelViewSet):
 
 	# 대댓글 생성
 	def create(self, request, *args, **kwargs):
-		serializer = self.get_serializer(data=request.data)
+		try:
+			serializer = self.get_serializer(data=request.data)
+			if serializer.is_valid():
+				# 대댓글을 달 post, comment 확인
+				post_id = kwargs["post_id"]
+				comment_id = kwargs["comment_id"]
 
-		if serializer.is_valid():
-			# 대댓글을 달 post, comment 확인
-			post_id = kwargs["post_id"]
-			comment_id = kwargs["comment_id"]
+				comment = Comment.objects.filter(id=comment_id, post_id=post_id, depth=1).first()
+				if comment:
+					sequence_max = Comment.objects.filter(group=comment.group, post_id=post_id).aggregate(
+						Max('sequence'))
+					sequence = sequence_max["sequence__max"] + 1
+					post = Post(id=post_id)
 
-			comment = Comment.objects.get(id=comment_id, post_id=post_id)
-			if comment:
-				sequence_max = Comment.objects.filter(group=comment.group, post_id=post_id).aggregate(Max('sequence'))
-				sequence = sequence_max["sequence__max"] + 1
-				post = Post(id=post_id)
-
-				Comment.objects.create(post=post, sequence=sequence, depth=2, group=comment.group, **request.data)
-				return Response(status=200, data=serializer.data)
+					Comment.objects.create(post=post, sequence=sequence, depth=2, group=comment.group, **request.data)
+					return Response(status=200, data=serializer.data)
+				else:
+					# 데이터 없음
+					return Response(status=400, data="데이터 없음")
 			else:
-				# 데이터 없음
 				return Response(status=400)
-		else:
-			return Response(status=400)
+		except Exception as e:
+			print(e)
+			return Response(status=500)
